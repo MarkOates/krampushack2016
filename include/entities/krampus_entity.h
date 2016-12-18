d97891d4c5a680cb5c79969a0efa58a14138c04d
Mark Oates <euph_boy@hotmail.com>
2016-12-18 11:53:26 -0500

    Add states to KrampusEntity

--- include/entities/krampus_entity.h
+++ include/entities/krampus_entity.h
@@ -10,7 +10,22 @@
 class KrampusEntity : public EntityBase
 {
 public:
+   enum state_t
+   {
+      WALKING_UP,
+      WALKING_DOWN,
+      WALKING_RIGHT,
+      WALKING_LEFT,
+      STANDING,
+      ATTACKING,
+      DEAD
+   };
+   int state;
+
+public:
    KrampusEntity(ElementID *parent, SpriteSheet *sprite_sheet, float x, float y);
+
+   void set_state(state_t new_state);
 };
 
 
--- src/entities/krampus_entity.cpp
+++ src/entities/krampus_entity.cpp
@@ -7,12 +7,23 @@
 
 KrampusEntity::KrampusEntity(ElementID *parent, SpriteSheet *sprite_sheet, float x, float y)
    : EntityBase(parent, "krampus", x, y)
+   , state(STANDING)
 {
    place.size = vec2d(120, 30);
    bitmap.bitmap(sprite_sheet->get_sprite(18));
    bitmap.align(0.5, 1.0);
    bitmap.scale(2.0, 2.0);
 }
+
+
+
+void KrampusEntity::set_state(state_t new_state)
+{
+   state = new_state;
+}
+
+
+
 
 
 

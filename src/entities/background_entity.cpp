3b33c58c91ccd8516ffa8c74a8ff42cd996f1e4f
Mark Oates <euph_boy@hotmail.com>
2016-12-22 21:15:02 -0500

    Move scaled render to factory

--- src/entities/background_entity.cpp
+++ src/entities/background_entity.cpp
@@ -3,16 +3,13 @@
 
 #include <entities/background_entity.h>
 
-#include <allegro_flare/image_processing.h>
-
 
 
 BackgroundEntity::BackgroundEntity(ElementID *parent, ALLEGRO_BITMAP *al_bitmap, float x, float y)
    : EntityBase(parent, "background", x, y)
 {
    place.align = vec2d(0.0, 0.0);
-   ALLEGRO_BITMAP *scaled_bitmap = create_pixel_perfect_scaled_render(al_bitmap, 3);
-   bitmap.bitmap(scaled_bitmap);
+   bitmap.bitmap(al_bitmap);
 }
 
 
--- src/factories/scene_factory.cpp
+++ src/factories/scene_factory.cpp
@@ -4,6 +4,7 @@
 #include <factories/scene_factory.h>
 
 #include <allegro_flare/framework.h>
+#include <allegro_flare/image_processing.h>
 #include <factories/entity_factory.h>
 #include <entities/entity_base.h>
 
@@ -12,14 +13,20 @@
 Scene *SceneFactory::create_test_scene()
 {
    Scene *scene = new Scene();
-   ALLEGRO_BITMAP *background_image = Framework::bitmap("background-1-02.png");
+
+   // background
+   // note, this bitmap dangles, and is never deleted
+   ALLEGRO_BITMAP *original_bitmap = Framework::bitmap("background-1-02.png");
+   ALLEGRO_BITMAP *scaled_background = create_pixel_perfect_scaled_render(original_bitmap, 5);
 
    for (unsigned i=0; i<2; i++)
-      EntityFactory::create_background(scene, background_image, i*480, 100);
+      EntityFactory::create_background(scene, scaled_background, i*al_get_bitmap_width(scaled_background), 100);
 
+   // enemies
    for (unsigned i=0; i<20; i++)
       EntityFactory::create_random_kid(scene, random_float(200, 900), random_float(400, 600));
 
+   // player
    EntityFactory::create_krampus(scene, 300, 600);
 
    return scene;

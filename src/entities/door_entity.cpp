99df4798a2b4ddf60f26dbffc682aacbc59b3b82
Mark Oates <euph_boy@hotmail.com>
2016-12-23 15:20:42 -0500

    Improve size and positioning of Door

--- src/entities/door_entity.cpp
+++ src/entities/door_entity.cpp
@@ -9,6 +9,9 @@ DoorEntity::DoorEntity(ElementID *parent, ALLEGRO_BITMAP *door_img, float x, flo
    : EntityBase(parent, "door", x, y)
 {
    bitmap.bitmap(door_img);
+   bitmap.align(0.5, 1.0);
+   bitmap.scale(1.5, 1.2);
+   place.size = vec2d(80, 5);
 }
 
 

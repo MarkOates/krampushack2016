9827649f43996cfac3f46c7d0a1dc0b044553c6e
Mark Oates <euph_boy@hotmail.com>
2016-12-26 15:52:09 -0500

    Enable KidEntity to reveal its naughty/nice behavior

--- include/entities/kid_entity.h
+++ include/entities/kid_entity.h
@@ -21,10 +21,16 @@ class KidEntity : public EntityBase
 private:
    behavior_t behavior;
    std::string name;
+   float identity_counter;
+   ALLEGRO_COLOR get_identity_color();
 
 public:
    KidEntity(ElementID *parent, SpriteSheet *sprite_sheet, float x, float y, std::string name, behavior_t behavior);
    virtual ~KidEntity();
+
+   void update() override;
+
+   void reveal_identity();
 };
 
 
--- src/entities/kid_entity.cpp
+++ src/entities/kid_entity.cpp
@@ -9,6 +9,7 @@ KidEntity::KidEntity(ElementID *parent, SpriteSheet *sprite_sheet, float x, floa
    : EntityBase(parent, "kid", x, y)
    , name(name)
    , behavior(behavior)
+   , identity_counter(0.0)
 {
    place.size = vec2d(60, 30);
    bitmap.bitmap(sprite_sheet->get_sprite(random_int(0, 16)));
@@ -22,6 +23,48 @@ KidEntity::KidEntity(ElementID *parent, SpriteSheet *sprite_sheet, float x, floa
 
 KidEntity::~KidEntity()
 {
+}
+
+
+
+void KidEntity::reveal_identity()
+{
+   identity_counter = 0.0;
+}
+
+
+
+void KidEntity::update()
+{
+   identity_counter += 1.0 / 60.0;
+
+   ALLEGRO_COLOR identity_color = get_identity_color();
+   ALLEGRO_COLOR normal_color = color::white;
+   float color_blend_ammt = std::min(identity_counter*0.25, 1.0);
+
+   bitmap.color(color::mix(identity_color, normal_color, color_blend_ammt));
+}
+
+
+
+ALLEGRO_COLOR KidEntity::get_identity_color()
+{
+   ALLEGRO_COLOR identity_color;
+
+   switch(behavior)
+   {
+   case BEHAVIOR_ADULT:
+      identity_color = color::gray;
+      break;
+   case BEHAVIOR_NAUGHTY:
+      identity_color = color::red;
+      break;
+   case BEHAVIOR_NICE:
+      identity_color = color::white;
+      break;
+   }
+
+   return identity_color;
 }
 
 

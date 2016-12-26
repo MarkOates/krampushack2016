34ac4b4cdf34a4de98751d10e4c57096b24d9eda
Mark Oates <euph_boy@hotmail.com>
2016-12-26 02:44:40 -0500

    Add door2

--- include/factories/entity_factory.h
+++ include/factories/entity_factory.h
@@ -19,6 +19,7 @@ private:
    ALLEGRO_BITMAP *background5;
    ALLEGRO_BITMAP *background6;
    ALLEGRO_BITMAP *door1;
+   ALLEGRO_BITMAP *door2;
    ALLEGRO_BITMAP *club_item;
    static EntityFactory *instance;
    static EntityFactory *get_instance();
@@ -31,6 +32,7 @@ public:
    static EntityBase *create_krampus(ElementID *parent, float x, float y);
    static EntityBase *create_krampus_attack_damage_zone(ElementID *parent, float x, float y, float w, float h);
    static EntityBase *create_door1(ElementID *parent, float x, float y, std::string name, int destination_scene, std::string destination_door_name);
+   static EntityBase *create_door2(ElementID *parent, float x, float y, std::string name, int destination_scene, std::string destination_door_name);
    static EntityBase *create_club_item(ElementID *parent, float x, float y);
    static EntityBase *create_stone_of_defiance_item(ElementID *parent, float x, float y);
    static EntityBase *create_naughty_list_item(ElementID *parent, float x, float y);
--- src/factories/entity_factory.cpp
+++ src/factories/entity_factory.cpp
@@ -31,6 +31,7 @@ EntityFactory::EntityFactory()
    , background5(nullptr)
    , background6(nullptr)
    , door1(nullptr)
+   , door2(nullptr)
 {
    background1 = create_pixel_perfect_scaled_render(Framework::bitmap("background-1-08.png"), 5);
    background2 = create_pixel_perfect_scaled_render(Framework::bitmap("background-2-02.png"), 5);
@@ -39,6 +40,7 @@ EntityFactory::EntityFactory()
    background5 = create_pixel_perfect_scaled_render(Framework::bitmap("background-5-01.png"), 5);
    background6 = create_pixel_perfect_scaled_render(Framework::bitmap("background-6-02.png"), 5);
    door1 = create_pixel_perfect_scaled_render(Framework::bitmap("door-1-02.png"), 5);
+   door2 = create_pixel_perfect_scaled_render(Framework::bitmap("door-2-01.png"), 5);
 }
 
 
@@ -109,6 +111,13 @@ EntityBase *EntityFactory::create_background6(ElementID *parent, float x, float 
 EntityBase *EntityFactory::create_door1(ElementID *parent, float x, float y, std::string name, int destination_scene, std::string destination_door_name)
 {
    return new DoorEntity(parent, name, destination_scene, destination_door_name, get_instance()->door1, x, y);
+}
+
+
+
+EntityBase *EntityFactory::create_door2(ElementID *parent, float x, float y, std::string name, int destination_scene, std::string destination_door_name)
+{
+   return new DoorEntity(parent, name, destination_scene, destination_door_name, get_instance()->door2, x, y);
 }
 
 

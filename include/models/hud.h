c066df1423009ba988adb898805c30eb8fe9fe63
Mark Oates <euph_boy@hotmail.com>
2016-12-28 03:56:49 -0500

    Create CINEMA and GAME_PLAY modes for HUD

--- include/models/hud.h
+++ include/models/hud.h
@@ -10,20 +10,33 @@
 class HUD
 {
 private:
+   enum mode_t
+   {
+      MODE_GAME_PLAY,
+      MODE_CINEMA
+   };
+
    Inventory *player_inventory;
    NaughtyList *naughty_list;
    int player_health;
    int player_max_health;
 
+   mode_t mode;
+   void set_mode(mode_t new_mode);
    BitmapObject chrome_bitmap;
    ALLEGRO_FONT *font;
+   float black_bar_counter;
 
 public:
    HUD(Inventory *player_inventory, NaughtyList *naughty_list);
 
    void set_values(int player_health, int player_max_health);
 
+   void update();
    void draw();
+
+   void set_to_cinema_mode();
+   void set_to_game_play_mode();
 };
 
 
--- src/models/hud.cpp
+++ src/models/hud.cpp
@@ -16,12 +16,35 @@ HUD::HUD(Inventory *player_inventory, NaughtyList *naughty_list)
    , player_max_health(0)
    , chrome_bitmap(nullptr)
    , font(Framework::font("ChronoTrigger.ttf 40"))
+   , black_bar_counter(0)
 {
    ALLEGRO_BITMAP *bmp = create_pixel_perfect_scaled_render(Framework::bitmap("top_hud-01.png"), 5);
    chrome_bitmap.bitmap(bmp);
-   chrome_bitmap.align(0.5, 0.0);
-   chrome_bitmap.scale(0.95);
+
    chrome_bitmap.position(1280/2, 20);
+   chrome_bitmap.align(0.5, 2.0);
+   chrome_bitmap.scale(0.95);
+
+   set_to_game_play_mode();
+}
+
+
+
+void HUD::set_mode(mode_t new_mode)
+{
+   mode = new_mode;
+
+   switch(new_mode)
+   {
+   case MODE_GAME_PLAY:
+      chrome_bitmap.get_attr("align_y") = 0.0; //<- a really annoying way to have to do this
+      black_bar_counter = 0.0;
+      break;
+   case MODE_CINEMA:
+      chrome_bitmap.get_attr("align_y") = 2.0; //<- a really annoying way to have to do this
+      black_bar_counter = 1.0;
+      break;
+   };
 }
 
 
@@ -51,6 +74,27 @@ void HUD::draw()
    hud_str << "     Adults: " << naughty_list->get_num_alive_adults();
 
    al_draw_text(font, color::white, 10, 720-50, 0, hud_str.str().c_str());
+
+   // draw the black bars
+   float black_bar_height = 100;
+   float top_bar_y = 0 - black_bar_height * (1.0 - black_bar_counter);
+   float bottom_bar_y = 720-black_bar_height + black_bar_height * (1.0 - black_bar_counter);
+   al_draw_filled_rectangle(0, top_bar_y, 1280, top_bar_y+black_bar_height, color::black);
+   al_draw_filled_rectangle(0, bottom_bar_y, 1280, bottom_bar_y+black_bar_height, color::black);
+}
+
+
+
+void HUD::set_to_cinema_mode()
+{
+   set_mode(MODE_CINEMA);
+}
+
+
+
+void HUD::set_to_game_play_mode()
+{
+   set_mode(MODE_GAME_PLAY);
 }
 
 

b6fb5d1fb0fa0b0da94dcf786fce6ccd9055cda8
Mark Oates <euph_boy@hotmail.com>
2017-01-02 02:27:29 -0500

    Add some clarity to the opening instructions

--- src/factories/storyboard_factory.cpp
+++ src/factories/storyboard_factory.cpp
@@ -11,9 +11,10 @@ StoryboardScreen *StoryboardFactory::create_opening_storyboard_screen(Display *d
 
    pages.push_back("Oh no!\n\nThere has been a big Christmas mix up this year and Krampus accidentally got the wrong list of Naughty kids.");
    pages.push_back("What's even worse is that it's already Christmas Eve!\n\nKrampus needs to punish all the naughty kids or it'll be a Christmas disaster!");
-   pages.push_back("Your task:\n\nTravel around Krampus cavern, find out who the naughty kids are, and deliver their well deserved Christmas punishment.");
-   pages.push_back("Use the ARROW KEYS and SPACEBAR to play.  Once you get the legendary Stone of Defiance, you can activate it by pressing the R key.");
+   pages.push_back("Your task:\n\nTravel around Krampus Cavern, find out who the naughty kids are, and deliver their well deserved Christmas punishment.");
+   pages.push_back("\nUse the ARROW KEYS and SPACEBAR to play.  Once you get the legendary Stone of Defiance, you can activate it by pressing the R key.");
    pages.push_back("And one last thing!");
+   pages.push_back("\nYou won't be able to do much damage on your own. You'll need to find a proper weapon to inflict some REAL Christmas pain!");
    pages.push_back("Good Luck!");
 
    StoryboardScreen *storyboard_screen = new StoryboardScreen(display, pages);
